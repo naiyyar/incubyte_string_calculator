9295473b8640b23a34429e89f009f0bb23500e3e
Naiyyar <naiyyar.abbas512013@gmail.com>
2025-10-16 18:43:13 +0530

    HAndling different delimiter case

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,10 +5,17 @@ class StringCalculator
 
     return str.to_i if str.length == 1
 
+    delimiter = ','
+
+    if str.start_with?('//')
+      delimiter_str, str = str.split("\n")
+      delimiter = delimiter_str[2..]
+    end
+
     if str.include?("\n")
-      str.gsub("\n", ',').split(',').map(&:to_i).sum
+      str.gsub("\n", delimiter).split(delimiter).map(&:to_i).sum
     else
-      str.split(',').map(&:to_i).sum
+      str.split(delimiter).map(&:to_i).sum
     end
   end
 end
